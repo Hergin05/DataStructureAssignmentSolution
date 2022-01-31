bfa8bed519507d267e07e05ff68df6c40ba1ea43
Hergin05 <61073545+Hergin05@users.noreply.github.com>
2022-01-31 11:51:52 +0530

    Add files via upload

---
+++ Assignment3/src/com/greatLearning/question1/main/Main.java
@@ -0,0 +1,30 @@
+package com.greatLearning.question1.main;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import com.greatLearning.question1.service.Service;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("enter the total no of floors in the building");
+		
+		int noOfFloor = sc.nextInt();
+
+	    int[] floors = new int[noOfFloor];
+		
+	   
+		for(int i=0; i<noOfFloor; i++)  
+		{  
+		System.out.println("enter the floor size given on day : "+(i+1));
+		floors[i] = sc.nextInt(); 
+		}  
+		System.out.println();
+		
+		Service service = new Service();
+		service.printConstructionTable(floors, noOfFloor);
+	}
+}
